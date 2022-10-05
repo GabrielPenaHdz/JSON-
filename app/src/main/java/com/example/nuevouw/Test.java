c245e3867c15b4fa5f3db08087a074414058a645
GabrielPenaHdz <pena.hernandez.gabriel15@gmail.com>
2022-10-04 19:32:10 -0500

    first commit

---
+++ app/src/main/java/com/example/nuevouw/Test.java
@@ -0,0 +1,32 @@
+package com.example.nuevouw;
+
+public class Test {
+    public String getOla() {
+        return ola;
+    }
+
+    public void setOla(String ola) {
+        this.ola = ola;
+    }
+
+    public int getPe() {
+        return pe;
+    }
+
+    public void setPe(int pe) {
+        this.pe = pe;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+    String ola = "ola";
+    int pe = 3;
+    Date date = null;
+}
